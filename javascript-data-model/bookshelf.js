f78a31f3fc775a287bc90fe2000bf0ca5f81f304
Keenan Ng <keenanng.dev@gmail.com>
2021-10-18 13:07:41 -0700

    starter files

---
+++ javascript-data-model/bookshelf.js
@@ -0,0 +1,17 @@
+var bookshelf = [
+  {
+    isbn: '978-1491929483',
+    title: 'Introduction to JavaScript Object Notation: A To-the-Point Guide to JSON',
+    author: 'Lindsay Bassett'
+  },
+  {
+    isbn: '978-1484218624',
+    title: 'JSON Quick Syntax Reference',
+    author: 'Wallace Jackson'
+  },
+  {
+    isbn: '978-0692232699',
+    title: 'Build APIs You Won\'t Hate: Everyone and their dog wants an API, so you should probably learn how to build them',
+    author: 'Phil Sturgeon & Laura Bohill'
+  }
+];
